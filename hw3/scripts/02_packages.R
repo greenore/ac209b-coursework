e341e482542ed7c1ccfa5e594e5ffb6d835b6586
Tim Hagmann <tim.hagmann@gmail.com>
2017-02-23 09:37:17 +0100

    add hw3

--- hw3/scripts/02_packages.R
+++ hw3/scripts/02_packages.R
@@ -6,13 +6,12 @@ if (Sys.info()["sysname"] == "Windows" & Sys.getenv("JAVA_HOME") == "") {
 }
 
 ## CRAN packages
-tidyverse_packages <- c("tidyverse", "classInt")
-visualization_packages <- c("RColorBrewer", "ggbeeswarm", "pander", "scales",
-                            "gridExtra")
-modelling_packages <- c("splines", "gam", "boot")
-mapping_packages <- c("maptools", "rgeos", "rgdal")
+tidyverse_packages <- c("tidyverse")
+visualization_packages <- c("RColorBrewer", "pander", "scales",
+                            "gridExtra", "ggbeeswarm")
+modelling_packages <- c("e1071", "Brobdingnag", "MCMCpack", "MGLM")
 
-required_packages <- c(tidyverse_packages, visualization_packages, modelling_packages, mapping_packages)
+required_packages <- c(tidyverse_packages, visualization_packages, modelling_packages)
 packagesCRAN(required_packages, update = setMissingVar(var_name = "update_package", value = FALSE))
 
 ## Clear Workspace
