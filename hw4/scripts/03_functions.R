eeafb6d8ac7e1c4b50229aa303f7dcb40cf23aa8
Tim Hagmann <tim.hagmann@gmail.com>
2017-03-08 22:23:45 +0100

    add scale function

--- hw4/scripts/03_functions.R
+++ hw4/scripts/03_functions.R
@@ -5,6 +5,10 @@
 if (any(search() %in% "projEnvironment")) detach("projEnvironment")
 projEnvironment <- new.env()
 
+# function to scale a column vector between 0 and 1
+projEnvironment$scale <- function(x){
+  (x - min(x)) / (max(x) - min(x))
+}
 
 attach(projEnvironment)
 rm(projEnvironment)
